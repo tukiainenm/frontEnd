0927c5e7a4a3ab37a887cac6ba0c176aa4ba2404
tukiainenm <83721917+tukiainenm@users.noreply.github.com>
2022-12-04 14:49:25 +0200

    Todolist tehty

---
+++ src/components/Todolist.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+function Todolist() {
+  const [todo, setTodo] = useState({desc: '', date: ''});
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (event) => {
+    event.preventDefault();
+    setTodos([...todos, todo]);
+  }
+
+  const inputChanged = (event) => {
+    setTodo({...todo, [event.target.name]: event.target.value});
+  }
+
+  return (
+    <div>
+      <header>
+        <h1>Simple todolist</h1>
+      </header>
+      <form onSubmit={addTodo}>
+        <input type="date" name="date" value={todo.date} onChange={inputChanged} />
+        <input type="text" name="desc" value={todo.desc} onChange={inputChanged} />
+        <input type="submit" value="Add" />  
+      </form>
+
+      <table>
+        <tr>
+          <th>Description</th>
+          <th>Date</th>
+        </tr>
+        <tbody>
+          {
+            todos.map((todo, index) =>
+            <tr key={index}>
+              <td>{todo.desc}</td>
+              <td>{todo.date}</td>
+            </tr>
+            )
+          }
+        </tbody>
+      </table>   
+    </div>
+  );
+};
+
+export default Todolist;
